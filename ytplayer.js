95249c8a406a0823674bdbb9b5ae18d51b5489f4
Bazludowski <66746018+Bazludowski@users.noreply.github.com>
2021-02-10 22:26:36 +0100

    Delete ytplayer.js

--- ytplayer.js
+++
@@ -1,9 +0,0 @@
-const YTPlayer = require('yt-player')
-const player = new YTPlayer('#player', opts.width(500), opts.height(500), opts.autoplay(true), opts.annotations(false) )
-
-player.load('GKSRyLdjsPA')
-player.setVolume(100)
-
-player.on('playing', () => {
-    console.log(player.getDuration()) // => 351.521
-})
